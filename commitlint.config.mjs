4e96a0567367eac7e13da7232f651a8d7af40ac3
DartMoon <storogenkokirill@gmail.com>
2026-06-16 12:38:29 +0300

    chore: update placeholder text in page component

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -3,7 +3,7 @@ const Configuration = {
   rules: {
     'type-empty': [2, 'never'],
     'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'chore', 'build', 'ci', 'revert']],
-    'scope-empty': [2, 'never'],
+    // 'scope-empty': [2, 'never'],
     'scope-enum': [2, 'always', ['test', 'ui', 'storybook']],
   },
 };
